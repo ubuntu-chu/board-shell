3bf66b47fc16bda58de37ef4e37beedd0b5a7161
chum <chum@gmail.com>
2014-12-22 18:09:43 +0800

    解析rcS.board 80%

--- etc/board/lpc3250/boarddefine-utility.sh
+++ etc/board/lpc3250/boarddefine-utility.sh
@@ -11,7 +11,8 @@ boardname_define_file=/etc/board/rcS.board_new
 boardname_table_start="#boardname definition table -- start"
 boardname_table_end="#boardname definition tabel -- end"
 #暂时文件
-boardname_file="./boardname"
+boardname_file="/var/tmp/boardname_temp_file"
+temp_file="/var/tmp/temp_file"
 
 title_array=()
 boardname_array=()
@@ -54,7 +55,6 @@ paramvalue_array=()
 #参数key数组
 paramkey_array=()
 
-temp_file="temp"
 DEST_BOARD_INFO_FILE=$BOARD_INFO_SRC_FILE
 
 help(){
@@ -447,13 +447,17 @@ arrayindex_get hardwarevalue_array "$new_hardware"
 hardwareconfig_get ${hardwareconfig_array[$array_index]}
 
 #配置部分
-if [ ${#paramkey_array[*]} -ne 0 ]; then
+if [ ! -z "$assigned_board_name" ]; then
 	#通过参数进行配置
 	#判断是否有非法参数
 	i=0
+	configmatch_array=()
 	#遍历数组 要判断key 和 value
 	while :;
 	do
+		if [ $i -ge ${#paramkey_array[*]} ]; then
+			break
+		fi
 		#key的值 去除--前缀
 		key=`echo ${paramkey_array[$i]}|tr -d '-'`
 		value=${paramvalue_array[$i]}
@@ -476,6 +480,7 @@ if [ ${#paramkey_array[*]} -ne 0 ]; then
 					#value 合法判断
 					if [ "$value" = "$config_var" ]; then
 						is_find=2
+						configmatch_array[$i]="$index"
 						break
 					fi
 				done
@@ -496,10 +501,26 @@ if [ ${#paramkey_array[*]} -ne 0 ]; then
 		echo "new ${key}=${value}"
 		new_config_array_add "${key}=${value}"
 		i=$(($i + 1))
-		if [ $i -ge ${#paramkey_array[*]} ]; then
-			break
-		fi
 	done
+	#判断传入的形参值是否已经全部配置完所有参数 对于尚未配置的参数 使用默认配置
+	if [ ${#configmatch_array[*]} -ne ${#configname_array[*]} ]; then
+		#遍历默认参数列表 
+		index_list=`seq 0 $((${#configname_array[*]} - 1))`
+		debug echo "index_list=$index_list"
+		tr_list=`echo ${configmatch_array[*]} | tr -d ' '`
+		debug echo "tr_list=$tr_list"
+		if [ -z $tr_list ]; then
+			#defaut_list="$index_list"
+			defaut_list=`echo $index_list| tr '\n' ' '`
+		else
+			defaut_list=`echo $index_list | tr -d $tr_list`
+		fi
+		debug echo "default_list=$defaut_list"
+		for defaut_value in $defaut_list;
+		do
+			new_config_array_add "${configname_array[$defaut_value]}=${configdefault_array[$defaut_value]}"
+		done
+	fi
 else
 	echo ""
 	echo "step3:choose configuration"
@@ -589,208 +610,3 @@ rm -rf $temp_file
 rm -rf $boardname_file
 
 
-
-
-
-
-
-#sed -i "/^"${boardname_table_start}",/^"${boardname_table_end}"/ s/board_name=.*/board_name="${new_board_name}"/\
-#	;/^"${PREV_DEFINE_KEY}"={/,/^\}/ s/board_type=.*/board_type="${new_board_type}"/\
-#	;/^"${PREV_DEFINE_KEY}"={/,/^\}/ s/hardware=.*/hardware="${new_hardware}"/" $DEST_BOARD_INFO_FILE
-#new_board_name=
-#new_board_type=
-#new_hardware=
-#skip_key="skip"
-#manual_set=0
-#with_hardware=0
-#
-#
-#help(){
-#	echo "Usage                 : $0 [board_name] [hardware]"
-#	echo "Param board_name      : ${board_name_array[*]}"
-#	echo "Param hardware        : "
-#	for name in ${board_name_array[*]};
-#	do
-#		boarddefine_get $name
-#		echo "                        $name - (${default_hardware_list[*]})"
-#	done
-#	exit 1
-#
-#}
-#
-#if [ $# -ge 1 ]; then
-#
-#	if [ $1 = "help" ]; then
-#		help
-#	else
-#		echo "-------------manual change board define------------"
-#		manual_set=1
-#		new_board_name=$1
-#		var=$new_board_name
-#	fi
-#	if [ $# -eq 2 ]; then
-#		new_hardware=$2
-#		with_hardware=1
-#	fi
-#fi
-#
-#
-#if [ -z $new_board_name ]; then
-#	echo -n "" > $SHELL_PREV_DEFINE_SYS_FILE
-#	section_content_get $PREV_DEFINE_KEY $BOARD_INFO_SRC_FILE $SHELL_PREV_DEFINE_SYS_FILE
-#
-#	for file in $SHELL_PREV_DEFINE_SYS_FILE
-#	do
-#		while read line
-#		do
-#			debug echo $line
-#			#依据获取到的key=value键值对  生成一个本地变量  以便后面对其进行引用
-#			eval $line
-#		done  < $file
-#	done
-#
-#	echo "----------------current board define----------------"
-#	cat $SHELL_PREV_DEFINE_SYS_FILE
-#
-#	echo "----------------change board define----------------"
-#	echo ""
-#	echo "step1:choose board name"
-#
-#	while true
-#	do
-#		select var in "$skip_key" ${board_name_array[*]}; do
-#			break
-#		done
-#
-#		if [ ! -z $var ]; then
-#			if [ $skip_key = $var ]; then
-#				new_board_name=$board_name
-#			else
-#				new_board_name=$var
-#			fi
-#			break;
-#		fi
-#	done
-#fi
-#
-#echo ""
-#echo "new board name: $new_board_name"
-#boarddefine_get $new_board_name
-#new_board_type=${default_board_type}
-#echo "new board type: ${new_board_type}"
-#if [ $skip_key = $var ]; then
-#	default_hardware=$hardware
-#else
-#	default_hardware=${default_hardware_list[0]}
-#fi
-#
-#if [ $manual_set -eq 1 ]; then
-#	if [ $with_hardware -eq 0 ]; then
-#		new_hardware=$default_hardware
-#	else
-#		index=0 
-#		#检查hardware参数是否正确
-#		for var in ${default_hardware_list[*]};
-#		do
-#			if [ $new_hardware = $var ]; then
-#				break
-#			fi
-#			index=$(($index+1))
-#		done
-#		if [ $index = ${#default_hardware_list[*]} ]; then
-#			echo "new hardware is invalid!  valid hardware:(${default_hardware_list[*]})"
-#			exit 1
-#		fi
-#	fi
-#else
-#	echo "defaule hardware: ${default_hardware}"
-#	echo ""
-#fi
-#
-#
-#if [ -z $new_hardware ]; then
-#	echo "step2:choose hardware"
-#
-#	while true
-#	do
-#		select var in "$skip_key" ${default_hardware_list[*]}; do
-#			break
-#		done
-#
-#		if [ ! -z $var ]; then
-#			if [ $skip_key = $var ]; then
-#				new_hardware=$default_hardware
-#			else
-#				new_hardware=$var
-#			fi
-#			echo ""
-#			break;
-#		fi
-#	done
-#fi
-#
-#echo "new hardware: $new_hardware"
-#
-#echo "------------write back new board define------------"
-#
-#if [ $# -eq 3 ]; then
-#	DEST_BOARD_INFO_FILE=$3
-#else
-#	DEST_BOARD_INFO_FILE=$BOARD_INFO_SRC_FILE
-#fi
-#
-#
-#sed -i "/^"${PREV_DEFINE_KEY}"={/,/^\}/ s/board_name=.*/board_name="${new_board_name}"/\
-#	;/^"${PREV_DEFINE_KEY}"={/,/^\}/ s/board_type=.*/board_type="${new_board_type}"/\
-#	;/^"${PREV_DEFINE_KEY}"={/,/^\}/ s/hardware=.*/hardware="${new_hardware}"/" $DEST_BOARD_INFO_FILE
-#
-#
-##index=0
-##while true
-##do
-##	if [ ${board_name_array[$index]} = ${board_name} ]; then
-##		break;
-##	fi
-##	let "index = $index + 1"
-##	if [ $index -ge ${#board_name_array[*]} ]; then
-##		break
-##	fi
-##done
-##
-##if [ $index -ge ${#board_name_array[*]} ]; then
-##	echo "$board_name is invalid!"
-##	exit 3
-##fi
-##
-##board_type=${board_type_array[$index]}
-##echo "new board type: ${board_type}"
-##echo ""
-##echo "step2:choose hardware"
-#
-##echo "init hardware:${init_hardware_array[$index]}"
-##while true
-##do
-##	read -p "enter new hardware:" hardware
-##
-##	if [ -z $hardware ]; then
-##		hardware=${init_hardware_array[$index]}
-##		break
-##	fi
-##	case "$hardware" in  
-##		*[!0-9]* ) 
-##			echo "invalid input! please input again"
-##			continue
-##		;;  
-##		* ) 
-##			break
-##		;;
-##	esac
-##done
-##
-##echo "new hardware: $hardware"
-#
-#
-#
-#
-#
-#
